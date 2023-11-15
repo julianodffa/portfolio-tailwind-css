d3f32a12ae143ffbec22b46b5785fa18d3d91a79
Juliano Daffa Adytia <julianodaffaadytia@gmail.com>
2023-11-15 10:22:40 +0700

    Update: Dark Mode + Feature Revision

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,17 @@
 // Navbar Fixed
 window.onscroll = function () {
     const header = document.querySelector('header');
+    const toTop = document.querySelector("#to-top");
     const fixedNav = header.offsetTop;
 
     if (window.pageYOffset > fixedNav) {
-        header.classList.add("navbar-fixed");        
+        header.classList.add("navbar-fixed");
+        toTop.classList.remove("hidden");
+        toTop.classList.add("flex");
     } else {
-        header.classList.remove("navbar-fixed");        
+        toTop.classList.remove("flex");
+        toTop.classList.add("hidden");
+        header.classList.remove("navbar-fixed");
     }
 }
 
@@ -14,7 +19,38 @@ window.onscroll = function () {
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener("click", function() {
+hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("hamburger-active");
     navMenu.classList.toggle("hidden");
 });
+
+// Click Outside hamburger
+window.addEventListener("click", function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove("hamburger-active");
+        navMenu.classList.add("hidden");
+    }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+    if (darkToggle.checked) {
+        html.classList.add("dark");
+        localStorage.theme = "dark";
+    } else {
+        html.classList.remove("dark")
+        localStorage.theme = "light";
+
+    }
+});
+
+// Move Toggle Position based on Mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+
+}
